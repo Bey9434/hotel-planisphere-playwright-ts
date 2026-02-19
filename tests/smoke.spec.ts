31536a6fda53163caefc514b300f10fabb04e6e4
Bey9434 <aluca9434@gmail.com>
2026-02-19 23:48:04 +0900

    feat: スモークテストを更新

--- tests/smoke.spec.ts
+++ tests/smoke.spec.ts
@@ -1,9 +1,13 @@
 import { test, expect } from "@playwright/test";
 
-test("Smoke Test: トップページが表示されること", async ({ page }) => {
-  // Act: トップページに移動
-  await page.goto("/");
+test.describe("Smoke Test", () => {
+  test("Smoke Test: トップページが表示されること", async ({ page }) => {
+    // Act: トップページに移動
+    await page.goto("/");
 
-  // Assert: タイトルが期待通りであることを確認
-  await expect(page).toHaveTitle("HOTEL PLANISPHERE - テスト自動化練習サイト");
+    // Assert: タイトルが期待通りであることを確認
+    await expect(page).toHaveTitle(
+      "HOTEL PLANISPHERE - テスト自動化練習サイト",
+    );
+  });
 });
